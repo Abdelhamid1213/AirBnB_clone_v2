fad33aae487ad931df1fb6038f2808c8fb08a184
Abdelhamid <abdelhamidelmaazi@gmail.com>
2024-02-01 17:05:04 +0100

    Tasks

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,33 +1,46 @@
 #!/usr/bin/env bash
 # This script sets up the web server for the AirBnB clone project.
-# It installs Nginx, creates necessary directories, sets up a test HTML file,
-# creates a symbolic link, updates the Nginx configuration, and restarts the Nginx service.
-# The script should be run with sudo privileges.
-#
-# Usage: sudo ./0-setup_web_static.sh
-#
-# Note: Make sure to update the server configuration file if necessary.
-#
-# Dependencies: Nginx
-#
-# Exit Codes:
-#   0 - Success
-#   1 - Script should be run with sudo
-#
-# Example:
-#   sudo ./0-setup_web_static.sh
-
-if [ "$EUID" -ne 0 ]; then
-  echo "Please run this script with sudo."
-  exit 1
-fi
 
 apt -y update
 apt -y install nginx
 mkdir -p /data/web_static/releases/test
 mkdir -p /data/web_static/shared
-echo "<html><head></head><body>Holberton School</body></html>" > /data/web_static/releases/test/index.html
+
+echo "Holberton School" > /data/web_static/releases/test/index.html
 ln -sf /data/web_static/releases/test/ /data/web_static/current
-chown -R ubuntu:ubuntu /data/
-sed -i '/server {/a \ \ \ \ location \/hbnb_static {\n\ \ \ \ \ \ alias \/data\/web_static\/current\/;\n\ \ \ \ \ \ index index.html;\n\ \ \ \ }\n' /etc/nginx/sites-available/default
+
+chown -R ubuntu /data/
+chgrp -R ubuntu /data/
+
+CONFIG=\
+"server {
+    listen 80 default_server;
+    listen [::]:80 default_server;
+    root /var/www/html;
+    index index.html index.htm index.nginx-debian.html;
+    server_name malloc.tech www.malloc.tech;
+    add_header X-Served-By \$hostname;
+    error_page 404 /404.html;
+
+    location / {
+        try_files \$uri \$uri/ =404;
+    }
+
+    location = /404.html {
+        root /var/www/html;
+        internal;
+    }
+
+    location /hbnb_static {
+        alias /data/web_static/current/;
+        index index.html;
+    }
+
+    if (\$request_filename ~ redirect_me){
+        rewrite ^ https://www.youtube.com/watch?v=xvFZjo5PgG0 permanent;
+    }
+}"
+
+bash -c "echo -e '$CONFIG' > /etc/nginx/sites-enabled/default"
+
 service nginx restart
