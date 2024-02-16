f9ec4703de1aabc87cfc52747ba7a1988ebf2412
Abdelhamid <abdelhamidelmaazi@gmail.com>
2024-02-16 09:10:35 +0100

    Tasks

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,22 +1,14 @@
+#!/usr/bin/python3
 from flask import Flask
-
 app = Flask(__name__)
 
 
-class HelloWorld:
-    """A simple class representing the Hello World application."""
-
-    @staticmethod
-    @app.route('/', strict_slashes=False)
-    def hello():
-        """
-        Route function to display 'Hello HBNB!'.
-
-        Returns:
-            str: The message 'Hello HBNB!'.
-        """
-        return 'Hello HBNB!'
+@app.route("/", strict_slashes=False)
+def index():
+    """Route function to display 'Hello HBNB!' message."""
+    return "Hello HBNB!"
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
+    """Starts the Flask web application."""
     app.run()
