ae1e64dccdf519a2c878ac69d007c56359658455
Abdelhamid <abdelhamidelmaazi@gmail.com>
2024-02-16 09:07:50 +0100

    Tasks

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,19 +1,22 @@
-from flask import Flask, render_template
+from flask import Flask
+
 app = Flask(__name__)
 
 
-@app.route("/", strict_slashes=False)
-def index():
-    """
-    This is the index route of the Flask application.
-    It returns a string "Hello HBNB!" when accessed.
-    """
-    return "Hello HBNB!"
+class HelloWorld:
+    """A simple class representing the Hello World application."""
+
+    @staticmethod
+    @app.route('/', strict_slashes=False)
+    def hello():
+        """
+        Route function to display 'Hello HBNB!'.
+
+        Returns:
+            str: The message 'Hello HBNB!'.
+        """
+        return 'Hello HBNB!'
 
 
-if __name__ == "__main__":
-    """
-    This is the main entry point of the application.
-    It runs the Flask application on the local server.
-    """
+if __name__ == '__main__':
     app.run()
