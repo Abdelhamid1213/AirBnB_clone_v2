6ef1285d5dccd46ccd30096bfd7eaccc925c81e7
Abdelhamid <abdelhamidelmaazi@gmail.com>
2024-02-16 09:34:25 +0100

    Tasks

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""A simple Flask web application."""
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def index():
+    """Route function to display 'Hello HBNB!' message."""
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def hbnb():
+    """Route function to display 'HBNB' message."""
+    return "HBNB"
+
+
+@app.route("/c/<text>", strict_slashes=False)
+def c(text):
+    """Route function to display '/c/<text>' message."""
+    return "C " + text.replace('_', ' ')
+
+
+@app.route('/python/', defaults={'text': 'is_cool'}, strict_slashes=False)
+@app.route("/python/<text>", strict_slashes=False)
+def python(text="is cool"):
+    """Route function to display 'python/<text>' message."""
+    return "Python " + text.replace('_', ' ')
+
+
+if __name__ == "__main__":
+    """Starts the Flask web application."""
+    app.run(host='0.0.0.0', port=5000)
