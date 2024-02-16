4eaf3081a6854f9f7a0c260b89694a79edf1bc7a
Abdelhamid <abdelhamidelmaazi@gmail.com>
2024-02-16 09:18:36 +0100

    Tasks

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""A simple Flask web application that displays 'Hello HBNB!' message."""
+"""A simple Flask web application."""
 from flask import Flask
 app = Flask(__name__)
 
@@ -8,6 +8,11 @@ app = Flask(__name__)
 def index():
     """Route function to display 'Hello HBNB!' message."""
     return "Hello HBNB!"
+
+@app.route("/hbnb", strict_slashes=False)
+def hbnb():
+    """Route function to display 'HBNB' message."""
+    return "HBNB"
 
 
 if __name__ == "__main__":
